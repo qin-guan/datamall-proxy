f9da55294828cac5cfe4bfef43183d9326680499
Qin Guan <qinguan20040914@gmail.com>
2023-09-03 11:35:07 +0800

    feat: add datamall proxy rule

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,2 +1,9 @@
 //https://nitro.unjs.io/config
-export default defineNitroConfig({});
+export default defineNitroConfig({
+  routeRules: {
+    '/**': {
+      proxy: 'http://datamall2.mytransport.sg/**'
+    }
+  }
+});
+
